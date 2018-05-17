47e26f4a1828745baa0447ae10092381abdd1372
林伟伟 <linweiwei@xiaoyouzi.com>
2018-05-17 17:45:07 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,21 @@
+
+## 按照与启动
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+## 依赖模块说明
+
+1. http模块使用 [fly.js](https://github.com/wendux/fly/blob/master/README-CH.md) （为什么选用fly.js，目前微信小程序的http模块质量偏低，fly貌似还可以，后面自己封装）
+2.
---
+++ src/pages/index/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+
+    <div class="userinfo" @click="bindViewTap">
+      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
+      <div class="userinfo-nickname">
+        <card :text="userInfo.nickName"></card>
+      </div>
+    </div>
+
+    <div class="usermotto">
+      <div class="user-motto">
+        <card :text="motto"></card>
+      </div>
+    </div>
+
+    <form class="form-container">
+      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
+      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
+    </form>
+    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+import thor from '../../common/thor/thor'
+export default {
+  data () {
+    return {
+      motto: 'Hello World',
+      userInfo: {}
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      wx.navigateTo({ url })
+    },
+    getUserInfo () {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+    },
+    clickHandle (msg, ev) {
+      console.log('clickHandle:', msg, ev)
+    },
+    searchCity (){
+      thor.get('http://result.eolinker.com/wYP6JLz90f26fac05e677d66cf8a93dd0771d41f4c8d8ca?uri=apis/hotel/search?cityName=福州keyword=万达')
+        .then(res=>{
+          console.log('请求成功', res);
+        })
+        .catch(err=>{
+          console.log('请求失败',err);
+        })
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo();
+    this.searchCity()
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+
+.counter {
+  display: inline-block;
+  margin: 10px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
